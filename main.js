07990f7d32d17c1635a531a1c92b4d82d13a3c31
piotrpardel15 <71120206+piotrpardel15@users.noreply.github.com>
2021-04-30 15:17:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function menu(menu) {
+    const wybraneDanie = menu
+    console.log(wybraneDanie)
+}
+
+function contact(contact) {
+    const daneKontaktowe = contact
+    console.log(daneKontaktowe)
+}
+
+
